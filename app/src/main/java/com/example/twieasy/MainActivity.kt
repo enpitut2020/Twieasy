94b7806257559f2a80165e252bfcfae6b205d2cc
LesserCold <gulhanguang@gmail.com>
2020-10-16 18:04:54 +0900

    初期UI

--- app/src/main/java/com/example/twieasy/MainActivity.kt
+++ app/src/main/java/com/example/twieasy/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.twieasy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.twieasy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener { jumpToLogin() }
+    }
+
+    private fun jumpToLogin(){
+        setContentView(R.layout.login_page)
     }
 }
