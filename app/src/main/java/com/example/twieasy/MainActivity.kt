d8a0e31d81851429e0b210abd617d916d55a0f63
LesserCold <gulhanguang@gmail.com>
2020-10-23 17:02:03 +0900

    review and chart

--- app/src/main/java/com/example/twieasy/MainActivity.kt
+++ app/src/main/java/com/example/twieasy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.twieasy
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.twieasy.databinding.ActivityMainBinding
 
@@ -16,7 +18,36 @@ class MainActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener { jumpToLogin() }
     }
 
+    private val reviews : List<String> = mutableListOf("楽単!", "落単!", "普通!", "Easy!","a","b","c","d","e")
+    private var page : Int = 1
     private fun jumpToLogin(){
-        setContentView(R.layout.login_page)
+        setContentView(R.layout.review)
+        val r1 : TextView = findViewById(R.id.review1)
+        val r2 : TextView = findViewById(R.id.review2)
+        val r3 : TextView = findViewById(R.id.review3)
+        val r4 : TextView = findViewById(R.id.review4)
+        val revColumn : List<TextView> = listOf(r1,r2,r3,r4)
+        showReview(page,revColumn)
+        val pre : Button = findViewById(R.id.pre)
+        val next : Button = findViewById(R.id.next)
+        next.setOnClickListener {
+            if(page <= reviews.size / 4)
+                showReview(++page, revColumn)
+        }
+        pre.setOnClickListener {
+            if(page != 1)
+                showReview(--page,revColumn)
+        }
     }
+
+    private fun showReview(page : Int, revColumn : List<TextView>){
+        for(num in (page-1) * 4 until page*4) {
+            if(num >= reviews.size)
+                revColumn[num % 4].text = ""
+            else
+                revColumn[num % 4].text = reviews[num]
+        }
+
+    }
+
 }
