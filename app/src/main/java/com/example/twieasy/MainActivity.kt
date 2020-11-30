2409ab3f64866262c55b832d84a9da67f8b7a8c4
afsdfsas <kv333777@yahoo.co.jp>
2020-11-30 23:51:11 +0900

    new

--- app/src/main/java/com/example/twieasy/MainActivity.kt
+++ app/src/main/java/com/example/twieasy/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import android.synthetic.main.activity_main.*
 import com.example.twieasy.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.first_boot.*
 import org.jsoup.Jsoup
