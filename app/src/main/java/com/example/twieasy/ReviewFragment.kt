ef0858fe44a0b668e5fc797d9aa283b7ef21de72
gen0x39 <migesan0319@gmail.com>
2020-12-20 09:42:58 +0900

    UIの変更.

--- app/src/main/java/com/example/twieasy/ReviewFragment.kt
+++ app/src/main/java/com/example/twieasy/ReviewFragment.kt
@@ -64,7 +64,6 @@ class ReviewFragment : Fragment() {
             layout.addView(r)
         }
 
-        val goBackToSubjects: Button = vii.findViewById(R.id.back_button)
         val writeReview: Button = vii.findViewById(R.id.writeReview)
 
         writeReview.setOnClickListener {
