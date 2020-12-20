cae08a48eef1a4e43efe48a3e714cd4023889a51
LesserCold <gulhanguang@gmail.com>
2020-12-20 13:43:03 +0900

    fragment

--- app/src/main/java/com/example/twieasy/DepartmentFragment.kt
+++ app/src/main/java/com/example/twieasy/DepartmentFragment.kt
@@ -11,12 +11,24 @@ import kotlinx.android.synthetic.main.fragment_select_department.view.*
 
 
 class DepartmentFragment : Fragment() {
+
+    var button: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            button = it.getInt("buttonNum")
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         val view =  inflater.inflate(R.layout.fragment_select_department, container, false)
         view.departmentButton.setOnClickListener{
-            findNavController().navigate(R.id.action_departmentFragment_to_tutorialFragment)
+            val bundle:Bundle = Bundle()
+            bundle.putInt("buttonNum", button!!)
+            findNavController().navigate(R.id.action_departmentFragment_to_loadFragment,bundle)
         }
         // Inflate the layout for this fragment
         return view
