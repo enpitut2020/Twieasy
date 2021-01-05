350b29442cce61e6a7efa76c3404040ed679b189
ryohei <ryohei.imagawa@gmail.com>
2021-01-05 12:14:13 +0900

    ListViewで講義一覧表示

--- app/src/main/java/com/example/twieasy/SubjectFragment.kt
+++ app/src/main/java/com/example/twieasy/SubjectFragment.kt
@@ -1,5 +1,6 @@
 package com.example.twieasy
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -29,7 +30,7 @@ class SubjectFragment : Fragment() {
         subjectView = ViewModelProvider(requireActivity()).get(SubjectViewModel::class.java)
 
         val view =  inflater.inflate(R.layout.fragment_subject, container, false)
-        val rl : LinearLayout = view.findViewById(R.id.fragment_subject_linear)
+        //val rl : LinearLayout = view.findViewById(R.id.fragment_subject_linear)
 
         listView = ListView(requireContext())
         // 科目名の配列
@@ -71,7 +72,7 @@ class SubjectFragment : Fragment() {
         })
 
 
-            for (i in 1..subjectView.subjects.size) {//i = sizeも処理される
+        /*for (i in 1..subjectView.subjects.size) {//i = sizeも処理される
             val r: Button = Button(requireActivity())
             r.id = i
             r.text = subjectView.subjects[i - 1].name
@@ -92,7 +93,56 @@ class SubjectFragment : Fragment() {
 
                 findNavController().navigate(R.id.action_subjectFragment_to_reviewFragment,bundle)
             }
+        }*/
+
+        val subjectList = mutableListOf<MutableMap<String, Any>>()
+        for (i in 1..subjectView.subjects.size){
+            var sub : MutableMap<String, Any> = mutableMapOf("name" to subjectView.subjects[i - 1].name, "easiness" to subjectView.subjects[i - 1].easiness.toString())
+            subjectList.add(sub)
         }
+
+        val from = arrayOf("name", "easiness")
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        val adapter = SimpleAdapter(view.context, subjectList, android.R.layout.simple_list_item_2, from, to)
+        adapter.viewBinder = CustomViewBinder()
+        val lvSubjects = view.findViewById<ListView>(R.id.lvSubjects)
+        lvSubjects.adapter = adapter
+        lvSubjects.onItemClickListener = ListItemClickListener()
+
         return view
+    }
+
+
+    private inner class CustomViewBinder : SimpleAdapter.ViewBinder {
+        override fun setViewValue(view: View, data: Any, textRepresentation: String): Boolean {
+            when(view.id) {
+                android.R.id.text1 ->
+                    Log.d("dataa", data as String)
+                android.R.id.text2 -> {
+                    Log.d("data", data as String)
+                    val easiness = data.toInt()
+                    if (easiness >= 50)
+                        //view.setBackgroundColor(Color.parseColor("##00acFF"))
+                    view.setBackgroundResource(R.drawable.frame_style_finalraku)
+                    else
+                        //view.setBackgroundColor(Color.parseColor("#f28a2f"))
+                    view.setBackgroundResource(R.drawable.frame_style_finalpien)
+
+                    //val r2: TextView = TextView(context)
+                    //r2.text = "楽単率 " + subjectView.subjects[i-1].easiness.toString() + "%"
+                }
+            }
+
+            return false
+        }
+    }
+
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val bundle : Bundle = Bundle()
+            bundle.putInt("ID",position + 1)
+            findNavController().navigate(R.id.action_subjectFragment_to_reviewFragment,bundle)
+        }
     }
 }
