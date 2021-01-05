d5e3b6f8f071f2f639e6bfc7fee1a37dc10721a3
gen0x39 <migesan0319@gmail.com>
2021-01-05 16:06:55 +0900

    検索機能実装Done．

--- app/src/main/java/com/example/twieasy/SubjectFragment.kt
+++ app/src/main/java/com/example/twieasy/SubjectFragment.kt
@@ -12,7 +12,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_subject.view.*
 
-
 class SubjectFragment : Fragment() {
 
     lateinit var subjectView : SubjectViewModel
@@ -24,15 +23,11 @@ class SubjectFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-        // adapterを作成します
     ): View? {
         // Inflate the layout for this fragment
         subjectView = ViewModelProvider(requireActivity()).get(SubjectViewModel::class.java)
 
         val view =  inflater.inflate(R.layout.fragment_subject, container, false)
-        //val rl : LinearLayout = view.findViewById(R.id.fragment_subject_linear)
-
-
 
         for (i in 1..subjectView.subjects.size){
             var sub : MutableMap<String, Any> = mutableMapOf("name" to subjectView.subjects[i - 1].name, "easiness" to subjectView.subjects[i - 1].easiness.toString())
@@ -63,37 +58,39 @@ class SubjectFragment : Fragment() {
                 if(!newText.isEmpty()) {
                     val regex = Regex(newText)
                     result = searchList.filter { regex.containsMatchIn(it) }.toMutableList()
-                    // 検索結果にものが格納されているとき
+                    // 検索結果に文字が格納されているとき
                     if (!result.isEmpty()) {
-                        //result.map { Log.d("input", it) }
+                        val resultMap = mutableListOf<MutableMap<String, Any>>()
 
-                        val arrayAdapter = ArrayAdapter(
-                            view.context,
-                            android.R.layout.simple_list_item_1,
-                            result
-                        )
-                        val listView: ListView = view.findViewById<ListView>(R.id.lvSearch)
-                        listView.adapter = arrayAdapter
-                        listView.onItemClickListener = SearchListItemClickListener()
+                        for (i in 1..result.size){
+                            val subjectName = result[i - 1]
+                            var index = 0
 
-                        // 講義一覧を消す
-                        val subjectListEmpty = mutableListOf<MutableMap<String, Any>>()
-                        adapter = SimpleAdapter(view.context, subjectListEmpty, android.R.layout.simple_list_item_2, from, to)
+                            for(i in 1..subjectView.subjects.size) {
+                                if (subjectView.subjects[i-1].name == subjectName) {
+                                    index = i - 1
+                                    break
+                                }
+                            }
+
+                            var res : MutableMap<String, Any> = mutableMapOf("name" to subjectView.subjects[index].name, "easiness" to subjectView.subjects[index].easiness.toString())
+                            resultMap.add(res)
+                        }
+
+                        adapter = SimpleAdapter(view.context, resultMap, android.R.layout.simple_list_item_2, from, to)
+                        adapter.viewBinder = CustomViewBinder()
                         lvSubjects.adapter = adapter
-
+                        lvSubjects.onItemClickListener = SearchListItemClickListener()
+                    }
+                    // 一致する検索結果が存在しないとき
+                    else {
+                        var resultEmpty = mutableListOf<MutableMap<String, Any>>()
+                        adapter = SimpleAdapter(view.context, resultEmpty, android.R.layout.simple_list_item_2, from, to)
+                        lvSubjects.adapter = adapter
                     }
                 }
+                // 検索欄に入力がないとき
                 else {
-                    val result: MutableList<String> = mutableListOf()
-                    val arrayAdapter = ArrayAdapter(
-                        view.context,
-                        android.R.layout.simple_list_item_1,
-                        result
-                    )
-                    val listView: ListView = view.findViewById<ListView>(R.id.lvSearch)
-                    listView.adapter = arrayAdapter
-
-                    // もし消されてたら復活
                     adapter = SimpleAdapter(view.context, subjectList, android.R.layout.simple_list_item_2, from, to)
                     adapter.viewBinder = CustomViewBinder()
                     lvSubjects.adapter = adapter
@@ -107,17 +104,12 @@ class SubjectFragment : Fragment() {
                 return false
             }
         })
-
-
         return view
     }
-
 
     private inner class CustomViewBinder : SimpleAdapter.ViewBinder {
         override fun setViewValue(view: View, data: Any, textRepresentation: String): Boolean {
             when(view.id) {
-
-
                 android.R.id.text1 ->
                     Log.d("dataa", data as String)
 
@@ -131,12 +123,10 @@ class SubjectFragment : Fragment() {
                         //view.setBackgroundColor(Color.parseColor("#f28a2f"))
                     view.setBackgroundResource(R.drawable.frame_style_finalpien)
 
-                    //val r2: TextView = TextView(context)
-                    //r2.text = "楽単率 " + subjectView.subjects[i-1].easiness.toString() + "%"
+                    // val r2: TextView = TextView(context)
+                    // r2.text = "楽単率 " + subjectView.subjects[i-1].easiness.toString() + "%"
                 }
-
             }
-
             return false
         }
     }
@@ -159,7 +149,6 @@ class SubjectFragment : Fragment() {
                     break
                 }
             }
-
             val bundle : Bundle = Bundle()
             bundle.putInt("ID",index)
             findNavController().navigate(R.id.action_subjectFragment_to_reviewFragment,bundle)
